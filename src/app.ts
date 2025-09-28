97646acfb125149c880d48f6a3fe504cb33cdc5b
Sajjadhosenshohan <mdshohansajjad@gmail.com>
2025-09-29 01:01:26 +0600

    Remove commented localhost origins from allowed CORS origins

--- src/app.ts
+++ src/app.ts
@@ -18,8 +18,8 @@ app.use("/uploads", express.static(path.join(process.cwd(), "uploads")));
 
 
 const allowedOrigins = [
-  "http://localhost:3001",
-  "http://localhost:3000",
+  // "http://localhost:3001",
+  // "http://localhost:3000",
   "https://answersmart.ai",
   "answersmart.ai",
   "https://aibackend.answersmart.ai",
